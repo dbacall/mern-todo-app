d76dd93dd7fff46fb4ad5e275a7765157aa7daa8
dbacall <dbacall@hotmail.co.uk>
2020-03-19 14:04:42 +0000

    chore: set up backend using node, express, body-parser and cors

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -78,35 +78,35 @@ export default class CreateTodo extends Component {
                     <div className="form-group">
                         <div className="form-check form-check-inline">
                             <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityLow" 
-                                    value="Low"
-                                    checked={this.state.todo_priority==='Low'} 
-                                    onChange={this.onChangeTodoPriority}
-                                    />
+                                type="radio" 
+                                name="priorityOptions" 
+                                id="priorityLow" 
+                                value="Low"
+                                checked={this.state.todo_priority==='Low'} 
+                                onChange={this.onChangeTodoPriority}
+                            />
                             <label className="form-check-label">Low</label>
                         </div>
                         <div className="form-check form-check-inline">
                             <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityMedium" 
-                                    value="Medium" 
-                                    checked={this.state.todo_priority==='Medium'} 
-                                    onChange={this.onChangeTodoPriority}
-                                    />
+                                type="radio" 
+                                name="priorityOptions" 
+                                id="priorityMedium" 
+                                value="Medium" 
+                                checked={this.state.todo_priority==='Medium'} 
+                                onChange={this.onChangeTodoPriority}
+                            />
                             <label className="form-check-label">Medium</label>
                         </div>
                         <div className="form-check form-check-inline">
                             <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityHigh" 
-                                    value="High" 
-                                    checked={this.state.todo_priority==='High'} 
-                                    onChange={this.onChangeTodoPriority}
-                                    />
+                                type="radio" 
+                                name="priorityOptions" 
+                                id="priorityHigh" 
+                                value="High" 
+                                checked={this.state.todo_priority==='High'} 
+                                onChange={this.onChangeTodoPriority}
+                            />
                             <label className="form-check-label">High</label>
                         </div>
                     </div>
