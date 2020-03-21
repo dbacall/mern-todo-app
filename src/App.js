9986168223e55196a2df7dbb3fa02e7b4c597e31
dbacall <dbacall@hotmail.co.uk>
2020-03-21 11:56:03 +0000

    feat: add conditional rendering for when there are no todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import CreateTodo from './components/create-todo.component'
-import EditTodo from './components/edit-todo-list.component'
-import TodosList from './components/todos-list.component'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo-list.component";
+import TodosList from "./components/todos-list.component";
 import logo from "./logo.png";
 
 class App extends Component {
@@ -14,21 +14,36 @@ class App extends Component {
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
             </Link>
-            <Link to="/" className="navbar-brand" id="main-title">Todo App</Link>
+            <Link to="/" className="navbar-brand" id="main-title">
+              Todo App
+            </Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
-                  <Link to="/" className="nav-link" id="todos-list-nav-btn">Todos</Link>
+                  <Link to="/" className="nav-link" id="todos-list-nav-btn">
+                    Todos
+                  </Link>
                 </li>
                 <li className="navbar-item">
-                  <Link to="/create" className="nav-link" id="create-todo-nav-btn">Create Todo</Link>
+                  <Link
+                    to="/create"
+                    className="nav-link"
+                    id="create-todo-nav-btn"
+                  >
+                    Create Todo
+                  </Link>
                 </li>
               </ul>
             </div>
           </nav>
-          <br/>
+          <br />
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,51 +1,66 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Todo from './todo.component'
+import React, { Component } from "react";
+import axios from "axios";
+import Todo from "./todo.component";
+import { Link } from "react-router-dom";
 
 export default class TodosList extends Component {
   constructor(props) {
     super(props);
-    this.state = {todos: []};
+    this.state = {
+      todos: []
+    };
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/todos/')
+    axios
+      .get("http://localhost:4000/todos/")
       .then(response => {
         this.setState({ todos: response.data });
-      }) 
-      .catch(function(error) {
-        console.log(error)
       })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 
   componentDidUpdate() {
-    console.log(this.state)
+    console.log(this.state);
   }
 
   todoList() {
-    return this.state.todos.map(function(currentTodo, i){
-        return <Todo todo={currentTodo} key={i} />;
-    })
+    return this.state.todos.map(function(currentTodo, i) {
+      return <Todo todo={currentTodo} key={i} />;
+    });
+  }
+
+  renderTodos() {
+    if (this.state.todos.length === 0)
+      return (
+        <Link to="/create" className="btn btn-primary">
+          Create Todo
+        </Link>
+      );
+
+    return (
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Responsible</th>
+            <th>Priority</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>{this.todoList()}</tbody>
+      </table>
+    );
   }
 
   render() {
     return (
       <div>
         <h2>Todos List</h2>
-        <table className="table table-striped" style={{ marginTop: 20 }} >
-            <thead>
-                <tr>
-                    <th>Description</th>
-                    <th>Responsible</th>
-                    <th>Priority</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                { this.todoList() }
-            </tbody>
-        </table>
+        {this.renderTodos()}
       </div>
-    )
+    );
   }
 }
